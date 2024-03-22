0784c0a7e87c962a0a505de8448b0d81fb060d3c
Chan Yat Fu <chanyatfu0616@gmail.com>
2024-03-22 17:49:41 +0800

    Refactor inference.py

--- egs/visinger2/inference.py
+++ egs/visinger2/inference.py
@@ -20,7 +20,20 @@ import numpy as np
 import time
 import argparse
 
-def parse_label(hps, pho, pitchid, dur, slur, gtdur):
+def parse_file(label_list_path):
+    """
+    Returns: { filename: [phones, pitchid, dur, slur, gtdur] }
+    """
+    id2label = {}
+    with open(label_list_path, "r") as in_file:
+        for line in in_file.readlines():
+            fileid, txt, *label = line.split('|')
+            id2label[fileid] = label
+    label = {file_name: parse_label(hps, *label) for file_name, label in id2label.items()}
+    return label
+
+
+def parse_label(hps, pho, pitchid, dur, gtdur, slur):
     phos = []
     pitchs = []
     durs = []
@@ -48,12 +61,13 @@ def parse_label(hps, pho, pitchid, dur, slur, gtdur):
     gtdurs = torch.LongTensor(gtdurs)
     return phos, pitchs, durs, slurs, gtdurs
 
+
 def load_model(model_dir):
 
     # load config and model
     model_path = utils.latest_checkpoint_path(model_dir)
     config_path = os.path.join(model_dir, "config.json")
-    
+
     hps = utils.get_hparams_from_file(config_path)
 
     print("Load model from : ", model_path)
@@ -64,43 +78,44 @@ def load_model(model_dir):
     _ = utils.load_checkpoint(model_path, net_g, None)
     return net_g, hps
 
-def inference_label2wav(net_g, label_list_path, output_dir, hps, cuda_id=None):
 
-    id2label = {}
-    with open(label_list_path, "r") as in_file:
-        for line in in_file.readlines():
-            fileid, txt, phones, pitchid, dur, gtdur, slur = line.split('|')
-            id2label[fileid] = [phones, pitchid, dur, slur, gtdur]
+def inference_once(net_g, label, hps, cuda_id=None):
+    pho, pitchid, dur, slur = label
+    device = torch.device(f"cuda:{cuda_id}" if cuda_id != None else "cpu")
+    with torch.no_grad():
+        # data
+        pho_lengths = torch.LongTensor([pho.size(0)])
+        pho = pho.unsqueeze(0)
+        pitchid = pitchid.unsqueeze(0)
+        dur = dur.unsqueeze(0)
+        slur = slur.unsqueeze(0)
 
-    for file_name in tqdm(id2label.keys()):
-        pho, pitchid, dur, slur, gtdur = id2label[file_name]
-        pho, pitchid, dur, slur, gtdur = parse_label(hps, pho, pitchid, dur, slur, gtdur)
+        if(cuda_id != None):
+            net_g = net_g.to(device)
+            pho = pho.to(device)
+            pho_lengths = pho_lengths.to(device)
+            pitchid = pitchid.to(device)
+            dur = dur.to(device)
+            slur = slur.to(device)
 
-        with torch.no_grad():
+        # infer
+        o, _, _ = net_g.infer(pho, pho_lengths, pitchid, dur, slur)
+        audio = o[0,0].data.to(device).float().numpy()
+        audio = audio * 32768 #hps.data.max_wav_value
+        audio = audio.astype(np.int16)
+        return audio
 
-            # data
-            pho_lengths = torch.LongTensor([pho.size(0)])
-            pho = pho.unsqueeze(0)
-            pitchid = pitchid.unsqueeze(0)
-            dur = dur.unsqueeze(0)
-            slur = slur.unsqueeze(0)
 
-            if(cuda_id != None):
-                net_g = net_g.cuda(0)
-                pho = pho.cuda(0)
-                pho_lengths = pho_lengths.cuda(0)
-                pitchid = pitchid.cuda(0)
-                dur = dur.cuda(0)
-                slur = slur.cuda(0)
-            
-            # infer
-            o, _, _ = net_g.infer(pho, pho_lengths, pitchid, dur, slur)
-            audio = o[0,0].data.cpu().float().numpy()
-            audio = audio * 32768 #hps.data.max_wav_value
-            audio = audio.astype(np.int16)
-           
-            # save
+def inference_all(net_g, label_list_path, output_dir, hps, cuda_id=None, output_wav=True):
+    label = parse_file(label_list_path)
+    ret = {}
+    for file_name, label in tqdm(label.items()):
+        audio = inference_once(net_g, label, hps, cuda_id)
+        ret[file_name] = audio
+        if output_wav:
             write(os.path.join(output_dir, file_name.split('.')[0] + '.wav' ), hps.data.sample_rate, audio)
+    return ret
+
 
 if __name__ == "__main__":
 
@@ -118,5 +133,5 @@ if __name__ == "__main__":
     if(not os.path.exists(output_dir)):
         os.makedirs(output_dir)
     print("load model end!")
-    
-    inference_label2wav(model, input_dir, output_dir, hps, cuda_id=0)
+
+    audio = inference_all(model, input_dir, output_dir, hps)
